d24261cd6af4b8aaf3f7d58be43272ee820d6fd6
Luiz Oliveira <luiz54099326@aluno.ms.senac.br>
2025-04-01 19:54:59 -0400

    atualizzar

--- Telas/cadastro.py
+++ Telas/cadastro.py
@@ -1,6 +1,7 @@
 import re
 from PyQt6.QtWidgets import QWidget, QLabel, QLineEdit, QPushButton, QMessageBox
-from PyQt6.QtGui import  QIcon
+from PyQt6.QtGui import QIcon
+from PyQt6.QtCore import Qt
 from Database.database import conectar
 
 class TelaCadastro(QWidget):
@@ -9,53 +10,53 @@ class TelaCadastro(QWidget):
         self.tela_login = tela_login
         self.initUI()
         self.setWindowIcon(QIcon("Img/cadastro.png"))
+        self.showMaximized()
 
     def initUI(self):
         self.setWindowTitle("Cadastro")
-        self.setGeometry(100, 100, 450, 550)
         self.setStyleSheet("background-color: #D3D3D3;")
 
         self.label_usuario = QLabel("Usuário:", self)
-        self.label_usuario.setGeometry(75, 50, 300, 20)
-
         self.input_usuario = QLineEdit(self)
-        self.input_usuario.setGeometry(75, 70, 300, 30)
 
         self.label_email = QLabel("E-mail:", self)
-        self.label_email.setGeometry(75, 110, 300, 20)
-
         self.input_email = QLineEdit(self)
-        self.input_email.setGeometry(75, 130, 300, 30)
 
         self.label_cpf = QLabel("CPF (apenas números):", self)
-        self.label_cpf.setGeometry(75, 170, 300, 20)
-
         self.input_cpf = QLineEdit(self)
-        self.input_cpf.setGeometry(75, 190, 300, 30)
 
         self.label_senha = QLabel("Senha:", self)
-        self.label_senha.setGeometry(75, 230, 300, 20)
-
         self.input_senha = QLineEdit(self)
         self.input_senha.setEchoMode(QLineEdit.EchoMode.Password)
-        self.input_senha.setGeometry(75, 250, 300, 30)
 
         self.label_repetir_senha = QLabel("Repetir Senha:", self)
-        self.label_repetir_senha.setGeometry(75, 290, 300, 20)
-
         self.input_repetir_senha = QLineEdit(self)
         self.input_repetir_senha.setEchoMode(QLineEdit.EchoMode.Password)
-        self.input_repetir_senha.setGeometry(75, 310, 300, 30)
 
         self.btn_cadastrar = QPushButton("Cadastrar", self)
-        self.btn_cadastrar.setGeometry(75, 360, 300, 40)
         self.btn_cadastrar.setStyleSheet("background-color: green; color: white;")
         self.btn_cadastrar.clicked.connect(self.cadastrar_usuario)
 
         self.btn_voltar = QPushButton("Voltar", self)
-        self.btn_voltar.setGeometry(75, 420, 300, 30)
         self.btn_voltar.setStyleSheet("background-color: red; color: white;")
         self.btn_voltar.clicked.connect(self.voltar_tela_login)
+
+    def resizeEvent(self, event):
+        largura = self.width()
+        altura = self.height()
+
+        self.label_usuario.setGeometry(largura // 2 - 150, altura // 6, 300, 20)
+        self.input_usuario.setGeometry(largura // 2 - 150, altura // 6 + 20, 300, 30)
+        self.label_email.setGeometry(largura // 2 - 150, altura // 6 + 60, 300, 20)
+        self.input_email.setGeometry(largura // 2 - 150, altura // 6 + 80, 300, 30)
+        self.label_cpf.setGeometry(largura // 2 - 150, altura // 6 + 120, 300, 20)
+        self.input_cpf.setGeometry(largura // 2 - 150, altura // 6 + 140, 300, 30)
+        self.label_senha.setGeometry(largura // 2 - 150, altura // 6 + 180, 300, 20)
+        self.input_senha.setGeometry(largura // 2 - 150, altura // 6 + 200, 300, 30)
+        self.label_repetir_senha.setGeometry(largura // 2 - 150, altura // 6 + 240, 300, 20)
+        self.input_repetir_senha.setGeometry(largura // 2 - 150, altura // 6 + 260, 300, 30)
+        self.btn_cadastrar.setGeometry(largura // 2 - 150, altura // 6 + 310, 300, 40)
+        self.btn_voltar.setGeometry(largura // 2 - 150, altura // 6 + 360, 300, 30)
 
     def cadastrar_usuario(self):
         username = self.input_usuario.text().strip()
--- Telas/login.py
+++ Telas/login.py
@@ -1,5 +1,6 @@
 from PyQt6.QtWidgets import QWidget, QLabel, QLineEdit, QPushButton, QMessageBox
 from PyQt6.QtGui import QPixmap, QIcon
+from PyQt6.QtCore import Qt
 from Database.database import conectar
 from Telas.cadastro import TelaCadastro
 
@@ -8,38 +9,41 @@ class TelaLogin(QWidget):
         super().__init__()
         self.initUI()
         self.setWindowIcon(QIcon("Img/image.png"))
+        self.showMaximized()
 
     def initUI(self):
         self.setWindowTitle("Login Administrador")
-        self.setGeometry(100, 100, 400, 300)
         self.setStyleSheet("background-color: #D3D3D3;")
 
         self.label_icone = QLabel(self)
-        self.label_icone.setPixmap(QPixmap("user_icon.png")) 
-        self.label_icone.setGeometry(80, 10, 40, 40)
+        self.label_icone.setPixmap(QPixmap("Img/image.png"))
         self.label_icone.setScaledContents(True)
 
         self.label_titulo = QLabel("", self)
-        self.label_titulo.setGeometry(130, 20, 200, 30)
         self.label_titulo.setStyleSheet("font-size: 16px; font-weight: bold;")
 
         self.label_usuario = QLabel("Usuário:", self)
-        self.label_usuario.setGeometry(50, 60, 300, 20)
-
         self.input_usuario = QLineEdit(self)
-        self.input_usuario.setGeometry(50, 80, 300, 30)
 
         self.label_senha = QLabel("Senha:", self)
-        self.label_senha.setGeometry(50, 120, 300, 20)
-
         self.input_senha = QLineEdit(self)
         self.input_senha.setEchoMode(QLineEdit.EchoMode.Password)
-        self.input_senha.setGeometry(50, 140, 300, 30)
 
         self.btn_login = QPushButton("Login", self)
-        self.btn_login.setGeometry(50, 190, 300, 40)
         self.btn_login.setStyleSheet("background-color: green; color: white;")
         self.btn_login.clicked.connect(self.fazer_login)
+
+    def resizeEvent(self, event):
+        largura = self.width()
+        altura = self.height()
+
+        self.label_icone.setGeometry(largura // 2 - 50, altura // 6, 100, 100)
+        self.label_titulo.setGeometry(largura // 2 - 100, altura // 6 + 110, 200, 30)
+        self.label_usuario.setGeometry(largura // 2 - 150, altura // 3, 300, 20)
+        self.input_usuario.setGeometry(largura // 2 - 150, altura // 3 + 20, 300, 30)
+        self.label_senha.setGeometry(largura // 2 - 150, altura // 3 + 60, 300, 20)
+        self.input_senha.setGeometry(largura // 2 - 150, altura // 3 + 80, 300, 30)
+        self.btn_login.setGeometry(largura // 2 - 150, altura // 3 + 130, 300, 40)
 
     def fazer_login(self):
         usuario = self.input_usuario.text().strip()
